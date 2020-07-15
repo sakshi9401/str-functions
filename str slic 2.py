53d0d373798bef4305fc9dc582d6285aeb8198c1
sakshi9401 <68016527+sakshi9401@users.noreply.github.com>
2020-07-15 19:29:12 +0530

    Add files via upload

---
+++ str slic 2.py
@@ -0,0 +1,11 @@
+x = 'i like chocolates  '
+y = 'sheiscute200'
+z = 'i m {} years old'
+age = 19
+print(z.format(age))
+print(x+y,end ='\t')
+print('\n')
+print(x[0::2])
+print(y[-2:-5:-1])
+print(len(y))
+print(len(x))
