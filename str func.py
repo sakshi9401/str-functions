d25b5cd82b45837a3d4e108a6ae78b606fce80de
sakshi9401 <68016527+sakshi9401@users.noreply.github.com>
2020-07-15 19:07:00 +0530

    Add files via upload

---
+++ str func.py
@@ -0,0 +1,14 @@
+#some string functions
+mystr = 'I am sakshi raikwar'
+mystr1 = 'i am sakshi {} years old'
+age = 19
+print(mystr1.format(age))
+print(mystr.isalnum())
+print(mystr.isalpha())
+print(mystr.lower())
+print(mystr.upper())
+print(mystr.capitalize())
+print(mystr.count('a'))
+print(mystr.replace("i", 'o'))
+print(len(mystr))
+print(len(mystr1))
